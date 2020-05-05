56f0d3e5de3b71dac4f5be06ada5a6843f769b1d
Andreas Ley <mail@andreas-ley.com>
2020-05-05 15:08:48 +0200

    Better stack traces using boost::stacktrace

--- libs/metaHDL_core/source/metaHDL_core/utils/StackTrace.cpp
+++ libs/metaHDL_core/source/metaHDL_core/utils/StackTrace.cpp
@@ -3,81 +3,38 @@
 #include "Enumerate.h"
 #include "Range.h"
 
+#include <boost/format.hpp>
+
 #include <memory>
 #include <stdexcept>
-
-#if defined(_MSC_VER)
-
-#elif defined(__GNUC__) && defined(__linux__)
-
-#include <execinfo.h>
-#include <cxxabi.h>
-
-#else
-#error "Unsupported platform!"
-#endif
-
 
 
 namespace mhdl {
 namespace utils {
 
 
-#if defined(_MSC_VER)
-
-#pragma message ( "StackTrace not yet implemented" )
-
-/// @todo: look into stack tracing on windows
-    
-void StackTrace::record(size_t size, size_t skipTop) { }
-std::vector<std::string> StackTrace::formatEntries() const { return {"Not implemented"}; }
-
-    
-#elif defined(__GNUC__) && defined(__linux__)
-
-
 void StackTrace::record(size_t size, size_t skipTop) 
 { 
-    std::vector<void*> fullBuffer(size+skipTop);
-    size_t traceSize = backtrace(fullBuffer.data(), fullBuffer.size());
+    boost::stacktrace::stacktrace trace;
+
+    size_t traceSize = trace.size();
     if (traceSize > skipTop) {
         m_trace.resize(traceSize - skipTop);
         for (size_t i = skipTop; i < traceSize; i++)
-            m_trace[i-skipTop] = fullBuffer[i];
+            m_trace[i-skipTop] = trace[i];
     } else
         m_trace.clear();
 }
 
 std::vector<std::string> StackTrace::formatEntries() const 
 { 
-    std::unique_ptr<char*, decltype(&free)> buf(backtrace_symbols(m_trace.data(), m_trace.size()), &free);
-
-    if (buf == nullptr)
-        throw std::runtime_error("backtrace_symbols failed!");
-    
     std::vector<std::string> result;
     result.resize(m_trace.size());
-    for (auto i : Range(m_trace.size())) {
-        const char *src = buf.get()[i];
-#if 0
-        int status;
-        std::unique_ptr<char, decltype(&free)> demangled(abi::__cxa_demangle(src, 0, 0, &status), &free);
-        if (status != 0)
-            throw std::runtime_error("__cxa_demangle failed!");
-        
-        result[i] = demangled.get();
-#else
-        /// @todo: demangle and get file/line numbers
-        result[i] = src;
-#endif
-    }
+    for (auto i : Range(m_trace.size()))
+        result[i] = (boost::format("[%08X] %s - %s(%d)") % m_trace[i].address() % m_trace[i].name() % m_trace[i].source_file() % m_trace[i].source_line()).str();
     
     return result;
 }
-
-#else
-#error "Unsupported platform!"
-#endif
 
 std::ostream &operator<<(std::ostream &stream, const StackTrace &trace)
 {
--- libs/metaHDL_core/source/metaHDL_core/utils/StackTrace.h
+++ libs/metaHDL_core/source/metaHDL_core/utils/StackTrace.h
@@ -1,9 +1,13 @@
 #pragma once
 
 
+#include <boost/stacktrace.hpp>
+
+
 #include <vector>
 #include <string>
 #include <ostream>
+
 
 namespace mhdl {
 namespace utils {
@@ -14,10 +18,10 @@ class StackTrace
 {
     public:
         void record(size_t size, size_t skipTop);
-        const std::vector<void*> &getTrace() const { return m_trace; }
+        const std::vector<boost::stacktrace::frame> &getTrace() const { return m_trace; }
         std::vector<std::string> formatEntries() const;
     protected:
-        std::vector<void*> m_trace;
+        std::vector<boost::stacktrace::frame> m_trace;
 };
 
 std::ostream &operator<<(std::ostream &stream, const StackTrace &trace);
