3dca0f1a35d03d20d84feedf8edb79e75a14d555
mio <mio@mflour.com>
2020-05-06 22:09:17 +0200

    add ElementrayVector base class add setBit for elementrary vector

--- libs/metaHDL_core/source/metaHDL_core/frontend/BitVector.cpp
+++ libs/metaHDL_core/source/metaHDL_core/frontend/BitVector.cpp
@@ -2,6 +2,72 @@
 
 namespace mhdl::core::frontend {
     
+void ElementaryVector::resize(size_t width)
+{
+    MHDL_DESIGNCHECK_HINT(m_node->isOrphaned(), "Can not resize signal once it is connected (driving or driven).");
+
+    m_node->setConnectionType(getSignalType(width));
+}
+
+Bit ElementaryVector::operator[](size_t idx)
+{
+    hlim::Node_Rewire* node = DesignScope::createNode<hlim::Node_Rewire>(1);
+    node->recordStackTrace();
+
+    size_t w = m_node->getOutputConnectionType(0).width;
+    idx = (w + (idx % w)) % w;
+
+    hlim::Node_Rewire::RewireOperation rewireOp;
+    rewireOp.ranges.push_back({
+        .subwidth = 1,
+        .source = hlim::Node_Rewire::OutputRange::INPUT,
+        .inputIdx = 0,
+        .inputOffset = (size_t)idx,
+        });
+    node->setOp(std::move(rewireOp));
+
+    node->connectInput(0, { .node = m_node, .port = 0ull });
+
+    return Bit({ .node = node, .port = 0ull });
+}
+
+void ElementaryVector::setBit(size_t idx, const Bit& in)
+{
+    MHDL_DESIGNCHECK_HINT(getWidth() > idx, "Out of bounds vector element assignment.");
+
+    hlim::Node_Rewire* node = DesignScope::createNode<hlim::Node_Rewire>(2);
+    node->recordStackTrace();
+
+    node->connectInput(0, { .node = m_node, .port = 0 });
+    node->connectInput(1, { .node = in.getNode(), .port = 0 });
+
+    hlim::Node_Rewire::RewireOperation rewireOp;
+    if (idx != 0)
+        rewireOp.ranges.push_back({
+            .subwidth = idx,
+            .source = hlim::Node_Rewire::OutputRange::INPUT,
+        });
+
+    rewireOp.ranges.push_back({
+        .subwidth = 1,
+        .source = hlim::Node_Rewire::OutputRange::INPUT,
+        .inputIdx = 1,
+    });
+
+    if (idx + 1 != getWidth())
+    {
+        rewireOp.ranges.push_back({
+            .subwidth = getWidth() - idx - 1,
+            .source = hlim::Node_Rewire::OutputRange::INPUT,
+            .inputOffset = idx + 1,
+        });
+    }
+
+    m_node = DesignScope::createNode<hlim::Node_Signal>();
+    m_node->recordStackTrace();
+    m_node->setConnectionType(node->getOutputConnectionType(0));
+    m_node->connectInput({ .node = node, .port = 0 });
+}
 
 BitVector::BitVector(size_t width)
 { 
--- libs/metaHDL_core/source/metaHDL_core/frontend/BitVector.h
+++ libs/metaHDL_core/source/metaHDL_core/frontend/BitVector.h
@@ -28,20 +28,30 @@ struct Selection {
     static Selection StridedSlice(int offset, size_t size, int stride);
 };
 
+class ElementaryVector : public ElementarySignal
+{
+public:
+    using ElementarySignal::ElementarySignal;
+
+    virtual void resize(size_t width);
+
+    Bit operator[](size_t idx);
+
+    void setBit(size_t idx, const Bit& in);
+
+};
+
 /**
  * @todo write docs
  */
 template<typename FinalType>
-class BaseBitVector : public ElementarySignal
+class BaseBitVector : public ElementaryVector
 {
     public:
         using isBitVectorSignal = void;
         
         BaseBitVector(const FinalType &rhs) { assign(rhs); }
         
-        virtual void resize(size_t width);
-        
-        Bit operator[](size_t idx);
 
         FinalType operator()(int offset, size_t size) { return operator()(Selection::Range(offset, size)); }
         FinalType operator()(const Selection &selection) { }
@@ -49,43 +59,8 @@ class BaseBitVector : public ElementarySignal
         FinalType &operator=(const FinalType &rhs) { assign(rhs); return *this; }
     protected:
         BaseBitVector() = default;
-        BaseBitVector(const hlim::NodePort &port) : ElementarySignal(port) { }        
+        BaseBitVector(const hlim::NodePort &port) : ElementaryVector(port) { }
 };
-
-
-template<typename FinalType>
-void BaseBitVector<FinalType>::resize(size_t width)
-{
-    MHDL_DESIGNCHECK_HINT(m_node->isOrphaned(), "Can not resize signal once it is connected (driving or driven).");
-    
-    m_node->setConnectionType(getSignalType(width));    
-}
-
-
-template<typename FinalType>
-Bit BaseBitVector<FinalType>::operator[](size_t idx)
-{
-    hlim::Node_Rewire *node = DesignScope::createNode<hlim::Node_Rewire>(1);
-    node->recordStackTrace();
-    
-    size_t w = m_node->getOutputConnectionType(0).width;
-    idx = (w + (idx % w)) % w;
-    
-    hlim::Node_Rewire::RewireOperation rewireOp;
-    rewireOp.ranges.push_back({
-        .subwidth = 1,
-        .source = hlim::Node_Rewire::OutputRange::INPUT,
-        .inputIdx = 0,
-        .inputOffset = (size_t) idx,
-    });
-    node->setOp(std::move(rewireOp));
-    
-    node->connectInput(0, {.node = m_node, .port = 0ull});
-
-    return Bit({.node = node, .port = 0ull});
-}
-
-
 
 
 class BitVector : public BaseBitVector<BitVector>
