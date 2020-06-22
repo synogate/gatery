19f20342cc505638898b9a789b58ef5386302cec
Andreas Ley <mail@andreas-ley.com>
2020-06-22 15:11:48 +0200

    Signal generator

--- net/source/hcl/hlim/supportNodes/Node_SignalGenerator.cpp
+++ net/source/hcl/hlim/supportNodes/Node_SignalGenerator.cpp
@@ -1,1 +1,71 @@
 #include "Node_SignalGenerator.h"
+
+#include "../../utils/Range.h"
+
+namespace hcl::core::hlim {
+
+Node_SignalGenerator::Node_SignalGenerator(BaseClock *clk) {
+    m_clocks.resize(1);
+    attachClock(clk, 0);
+}
+
+void Node_SignalGenerator::setOutputs(const std::vector<ConnectionType> &connections)
+{
+    resizeOutputs(connections.size());
+    for (auto i : utils::Range(connections.size())) {
+        setOutputConnectionType(i, connections[i]);
+        setOutputType(i, OUTPUT_LATCHED);
+    }
+}
+
+void Node_SignalGenerator::resetDataDefinedZero(sim::DefaultBitVectorState &state, const size_t *outputOffsets) const
+{
+    for (auto i : utils::Range(getNumOutputPorts())) {
+        state.setRange(sim::DefaultConfig::VALUE, outputOffsets[i], getOutputConnectionType(i).width, false);
+        state.setRange(sim::DefaultConfig::DEFINED, outputOffsets[i], getOutputConnectionType(i).width, true);
+    }
+}
+
+void Node_SignalGenerator::resetDataUndefinedZero(sim::DefaultBitVectorState &state, const size_t *outputOffsets) const
+{
+    for (auto i : utils::Range(getNumOutputPorts())) {
+        state.setRange(sim::DefaultConfig::VALUE, outputOffsets[i], getOutputConnectionType(i).width, false);
+        state.setRange(sim::DefaultConfig::DEFINED, outputOffsets[i], getOutputConnectionType(i).width, false);
+    }
+}
+
+void Node_SignalGenerator::simulateReset(sim::DefaultBitVectorState &state, const size_t *internalOffsets, const size_t *outputOffsets) const
+{
+    std::uint64_t &tick = state.data(sim::DefaultConfig::VALUE)[internalOffsets[0]/64];
+    tick = 0;
+    produceSignals(state, outputOffsets, tick);
+}
+
+void Node_SignalGenerator::simulateAdvance(sim::DefaultBitVectorState &state, const size_t *internalOffsets, const size_t *outputOffsets, size_t clockPort) const
+{
+    std::uint64_t &tick = state.data(sim::DefaultConfig::VALUE)[internalOffsets[0]/64];
+    tick++;
+    produceSignals(state, outputOffsets, tick);
+}
+
+std::string Node_SignalGenerator::getTypeName() const 
+{ 
+    return "SignalGenerator";
+}
+
+void Node_SignalGenerator::assertValidity() const 
+{ 
+
+}
+
+std::string Node_SignalGenerator::getInputName(size_t idx) const 
+{
+    return "";
+}
+
+std::vector<size_t> Node_SignalGenerator::getInternalStateSizes() const 
+{
+    return {64};
+}
+
+}
--- net/source/hcl/hlim/supportNodes/Node_SignalGenerator.h
+++ net/source/hcl/hlim/supportNodes/Node_SignalGenerator.h
@@ -6,6 +6,23 @@ namespace hcl::core::hlim {
 
 class Node_SignalGenerator : public Node<Node_SignalGenerator>
 {
+    public:
+        Node_SignalGenerator(BaseClock *clk);
+        
+        virtual void simulateReset(sim::DefaultBitVectorState &state, const size_t *internalOffsets, const size_t *outputOffsets) const override;
+        virtual void simulateAdvance(sim::DefaultBitVectorState &state, const size_t *internalOffsets, const size_t *outputOffsets, size_t clockPort) const override;
+
+        virtual std::string getTypeName() const override;
+        virtual void assertValidity() const override;
+        virtual std::string getInputName(size_t idx) const override;
+
+        virtual std::vector<size_t> getInternalStateSizes() const override;
+    protected:
+        void setOutputs(const std::vector<ConnectionType> &connections);
+        void resetDataDefinedZero(sim::DefaultBitVectorState &state, const size_t *outputOffsets) const;
+        void resetDataUndefinedZero(sim::DefaultBitVectorState &state, const size_t *outputOffsets) const;
+        
+        virtual void produceSignals(sim::DefaultBitVectorState &state, const size_t *outputOffsets, size_t clockTick) const = 0;
 };
 
 }
