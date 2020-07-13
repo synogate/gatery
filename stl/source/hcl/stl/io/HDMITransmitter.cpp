c9ba18ddaaf983ee0e8f88c5c085f08fd04a1c42
mio <mio@mflour.com>
2020-07-13 18:02:35 +0200

    add tmds encoder interface prototype

--- stl/source/hcl/stl/io/HDMITransmitter.cpp
+++ stl/source/hcl/stl/io/HDMITransmitter.cpp
@@ -113,10 +113,11 @@ core::frontend::BitVector tmdsDecodeReduceTransitions(const core::frontend::BitV
 core::frontend::BitVector tmdsEncodeBitflip(const core::frontend::RegisterFactory& clk, const core::frontend::BitVector& data)
 {
     HCL_COMMENT << "count the number of uncompensated ones";
-    Register<UnsignedInteger> global_counter{ clk.config(), 0b000_uvec };
+    Register<UnsignedInteger> global_counter{ clk.config(), 0b0000_uvec };
     HCL_NAMED(global_counter);
 
-    UnsignedInteger word_counter = 0b100_uvec;
+    // TODO: depend with and start value on data width
+    UnsignedInteger word_counter = 0b1011_uvec; // -9 truncated
     for (size_t i = 0; i < data.getWidth(); ++i)
     {
         UnsignedInteger tmp{ 1 };
@@ -152,6 +153,114 @@ core::frontend::BitVector hdmi::tmdsDecodeBitflip(const core::frontend::BitVecto
     for (size_t i = 0; i < tmp.getWidth(); ++i)
         tmp.setBit(i, tmp[i] ^ data[data.getWidth() - 1]);
     return tmp;
+}
+
+TmdsEncoder::TmdsEncoder(core::frontend::RegisterFactory& clk) :
+    m_Clk{clk}
+{
+    m_Channel.fill(0b0010101011_vec); // no data symbol
+    m_Channel[0].setName("redChannel"); // TODO: convinient method to name arrays
+    m_Channel[0].setName("greenChannel");
+    m_Channel[0].setName("blueChannel");
+}
+
+void TmdsEncoder::addSync(const core::frontend::Bit& hsync, const core::frontend::Bit& vsync)
+{
+    IF(hsync)
+        setSync(true, false);
+    IF(vsync)
+        setSync(false, true);
+    IF(hsync && vsync)
+        setSync(true, true);
+}
+
+void hcl::stl::hdmi::TmdsEncoder::setColor(const ColorRGB& color)
+{
+    m_Channel[0] = tmdsEncodeBitflip(m_Clk, tmdsEncodeReduceTransitions(color.r));
+    m_Channel[1] = tmdsEncodeBitflip(m_Clk, tmdsEncodeReduceTransitions(color.g));
+    m_Channel[2] = tmdsEncodeBitflip(m_Clk, tmdsEncodeReduceTransitions(color.b));
+}
+
+void hcl::stl::hdmi::TmdsEncoder::setSync(bool hsync, bool vsync)
+{
+    if (hsync && vsync)
+        m_Channel[2] = 0b1010101011_vec;
+    else if (hsync)
+        m_Channel[2] = 0b0010101011_vec;
+    else if (vsync)
+        m_Channel[2] = 0b0101010100_vec;
+    else
+        m_Channel[2] = 0b0010101011_vec;
+}
+
+void hcl::stl::hdmi::TmdsEncoder::setTERC4(const core::frontend::BitVector& ctrl)
+{
+    const BitVector trec4lookup[] = {
+        0b1010011100_vec,
+        0b1001100011_vec,
+        0b1011100100_vec,
+        0b1011100010_vec,
+        0b0101110001_vec,
+        0b0100011110_vec,
+        0b0110001110_vec,
+        0b0100111100_vec,
+        0b1011001100_vec,
+        0b0100111001_vec,
+        0b0110011100_vec,
+        0b1011000110_vec,
+        0b1010001110_vec,
+        0b1001110001_vec,
+        0b0101100011_vec,
+        0b1011000011_vec
+    };
+
+    HCL_ASSERT(ctrl.getWidth() == 6);
+    //m_Channel[0] = mux(ctrl(0, 2), trec4lookup); // TODO: improve mux to accept any container as second input
+    //m_Channel[1] = mux(ctrl(2, 2), trec4lookup); // TODO: subrange as argument for mux
+    //m_Channel[2] = mux(ctrl(4, 2), trec4lookup);
+}
+
+SerialTMDS hcl::stl::hdmi::TmdsEncoder::serialOutput() const
+{
+    // TODO: use shift register/serdes lib for automatic vendor specific serdes usage
+
+    // TODO: create multiplied clock from m_Clk
+    RegisterFactory& fastClk = m_Clk;
+
+    Register<BitVector> chan[] = { 
+        {fastClk.config(), 0b0000000000_vec}, // TODO: Register<> without reset
+        {fastClk.config(), 0b0000000000_vec},
+        {fastClk.config(), 0b0000000000_vec}
+    };
+
+    for (auto& c : chan)
+        c = c.delay(1) >> 1;
+
+    Register<UnsignedInteger> shiftCounter{ fastClk.config(), 0x0_uvec };
+    HCL_NAMED(shiftCounter);
+    shiftCounter = shiftCounter.delay(1) + 1_uvec;
+
+    IF(shiftCounter == 9_uvec)
+    {
+        shiftCounter = 0x0_uvec;
+
+        for (size_t i = 0; i < m_Channel.size(); ++i)
+            chan[i] = m_Channel[i]; // TODO: clock domain crossing lib and warning
+    }
+
+    SerialTMDS out;
+    // TODO: convinient method to name signals in structs
+
+    out.clock.pos = true; // TODO: signal from clock
+    out.clock.neg = false;
+
+    for (size_t i = 0; i < m_Channel.size(); ++i)
+    {
+        out.data[i].pos = chan[i].lsb();
+        out.data[i].neg = ~chan[i].lsb();
+    }
+
+    return SerialTMDS();
 }
 
 }
--- stl/source/hcl/stl/io/HDMITransmitter.h
+++ stl/source/hcl/stl/io/HDMITransmitter.h
@@ -22,6 +22,53 @@ core::frontend::BitVector tmdsDecodeReduceTransitions(const core::frontend::BitV
 core::frontend::BitVector tmdsEncodeBitflip(const core::frontend::RegisterFactory& clk, const core::frontend::BitVector& data);
 core::frontend::BitVector tmdsDecodeBitflip(const core::frontend::BitVector& data);
 
+template<typename T>
+class UnpackScope : public core::frontend::ConditionalScope
+{
+public:
+    UnpackScope(const core::frontend::Bit& condition, T& obj) : ConditionalScope(condition), m_Obj(obj) {}
+    operator T& () { return m_Obj; }
+
+private:
+    T& m_Obj;
+};
+
+template<typename T>
+struct Valid : T
+{
+    core::frontend::Bit valid;
+
+    UnpackScope<T> unpack() { return { valid, *this }; }
+    UnpackScope<const T> unpack() const { return { valid, *this }; }
+};
+
+struct ColorRGB
+{
+    core::frontend::BitVector r, g, b;
+};
+
+class TmdsEncoder
+{
+public:
+    TmdsEncoder(core::frontend::RegisterFactory& clk);
+
+    void addColorStream(const Valid<ColorRGB>& color) { setColor(color.unpack()); }
+    void addSync(const core::frontend::Bit& hsync, const core::frontend::Bit& vsync);
+    void addTERC4(const Valid<core::frontend::BitVector>& ctrl) { setTERC4(ctrl.unpack()); }
+
+    // alternative interface using conditionals
+    void setColor(const ColorRGB& color);
+    void setSync(bool hsync, bool vsync);
+    void setTERC4(const core::frontend::BitVector& ctrl);
+
+    SerialTMDS serialOutput() const;
+    const auto& channels() const { return m_Channel; }
+
+protected:
+    core::frontend::RegisterFactory& m_Clk;
+    std::array<core::frontend::BitVector, 3> m_Channel;
+};
+
 class Transmitter
 {
     public:
