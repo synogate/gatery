f9e91318629692bc77666477877ccdea1445aa31
mio <mio@mflour.com>
2020-07-15 13:56:35 +0200

    add combinatorial gcd example

--- frontend/unitTests/source/fsm.cpp
+++ frontend/unitTests/source/fsm.cpp
@@ -160,3 +160,88 @@ BOOST_DATA_TEST_CASE_F(hcl::core::sim::UnitTestSimulationFixture, TestGCD, data:
 
     runTicks(design.getCircuit(), clk, maxTicks);
 }
+
+BOOST_DATA_TEST_CASE_F(hcl::core::sim::UnitTestSimulationFixture, FSMlessTestGCD, data::make({ 1, 2, 3, 4, 5, 10, 42 })* data::make({ 1, 2, 3, 4, 5, 23, 56, 126 }), x, y)
+{
+    using namespace hcl::core::frontend;
+
+    DesignScope design;
+
+    auto clk = design.createClock<hcl::core::hlim::RootClock>("clk", hcl::core::hlim::ClockRational(10'000));
+    RegisterConfig regConf = { .clk = clk, .resetName = "rst" };
+    RegisterFactory reg(regConf);
+
+    auto gcd_ref = [](unsigned a, unsigned b) -> unsigned {
+        while (a != b) {
+            if (a > b)
+                a -= b;
+            else
+                b -= a;
+        }
+        return a;
+    };
+
+
+    unsigned maxTicks = 200;
+
+    {
+        BVec x_vec = ConstBVec(x, 8);
+        BVec y_vec = ConstBVec(y, 8);
+
+        Bit start;
+        simpleSignalGenerator(clk, [](SimpleSignalGeneratorContext& context) {
+            context.set(0, context.getTick() == 0);
+            }, start);
+
+        BVec result;
+        Bit done = false;
+
+        {
+            HCL_NAMED(x_vec);
+            HCL_NAMED(y_vec);
+
+            GroupScope entity(hcl::core::hlim::NodeGroup::GRP_ENTITY);
+            entity
+                .setName("gcd")
+                .setComment("Statemachine to compute the GCD of two 8-bit integers.");
+
+
+            Register<BVec> a(regConf, 0b00000000_bvec);
+            a = a.delay(1);
+            Register<BVec> b(regConf, 0b00000000_bvec);
+            b = b.delay(1);
+
+            IF(start) {
+                a = x_vec;
+                b = y_vec;
+            }
+
+            IF(a == b) {
+                done = true;
+            } ELSE{
+                IF(a > b) {
+                    a = a - b;
+                } ELSE {
+                    b = b - a;
+                }
+            }
+
+            result = a;
+            HCL_NAMED(result);
+            HCL_NAMED(done);
+
+            //sim_debug() << "a is " << a.delay(1) << " and b is " << b.delay(1);
+        }
+
+        BVec ticks(8);
+        simpleSignalGenerator(clk, [](SimpleSignalGeneratorContext& context) {
+            context.set(0, context.getTick());
+            }, ticks);
+
+        sim_assert((ticks < ConstBVec(maxTicks - 1, 8)) || done) << "The state machine should be idle after " << maxTicks << " cycles";
+        BVec gtruth = ConstBVec(gcd_ref(x, y), 8);
+        sim_assert((ticks < ConstBVec(maxTicks - 1, 8)) || (result == gtruth)) << "The state machine computed " << result << " but the correct answer is " << gtruth;
+    }
+
+    runTicks(design.getCircuit(), clk, maxTicks);
+}
