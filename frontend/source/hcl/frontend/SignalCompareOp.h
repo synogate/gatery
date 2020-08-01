4433ef5763b8b904ef87c48ca85c55def424ddcf
mio <mio@mflour.com>
2020-08-01 20:47:08 +0200

    rewrite compare operators to use SignalPorts

--- frontend/source/hcl/frontend/SignalCompareOp.h
+++ frontend/source/hcl/frontend/SignalCompareOp.h
@@ -1,73 +1,49 @@
 #pragma once
 
 #include "Signal.h"
-#include "Scope.h"
+#include "SignalPort.h"
 #include "Bit.h"
 #include "BitVector.h"
 
 #include "SignalLogicOp.h"
 
-#include <hcl/hlim/coreNodes/Node_Signal.h>
 #include <hcl/hlim/coreNodes/Node_Compare.h>
 
 #include <hcl/utils/Preprocessor.h>
-#include <hcl/utils/Traits.h>
 
 namespace hcl::core::frontend {
 
-    class SignalCompareOp
+    inline hlim::NodePort makeNode(hlim::Node_Compare::Op op, const SignalPort& lhs, const SignalPort& rhs)
     {
-    public:
-        SignalCompareOp(hlim::Node_Compare::Op op) : m_op(op) { }
-
-        Bit operator()(const BVec& lhs, const BVec& rhs) { return create(lhs, rhs); }
-        Bit operator()(const Bit& lhs, const Bit& rhs) { return create(lhs, rhs); }
-
-        inline hlim::Node_Compare::Op getOp() const { return m_op; }
-    protected:
-        hlim::Node_Compare::Op m_op;
-        /// extend etc.
-        template<typename LhsSignalType, typename RhsSignalType>
-        Bit create(const LhsSignalType& lhs, const RhsSignalType& rhs);
-    };
-
-
-    template<typename LhsSignalType, typename RhsSignalType>
-    Bit SignalCompareOp::create(const LhsSignalType& lhs, const RhsSignalType& rhs) {
         HCL_DESIGNCHECK_HINT(lhs.getWidth() == rhs.getWidth(), "Signal comparison is needs equal width for non auto extendable types.");
 
-        auto* node = DesignScope::createNode<hlim::Node_Compare>(m_op);
+        auto* node = DesignScope::createNode<hlim::Node_Compare>(op);
         node->recordStackTrace();
         node->connectInput(0, lhs.getReadPort());
         node->connectInput(1, rhs.getReadPort());
 
-        return Bit({ .node = node, .port = 0 });
+        return { .node = node, .port = 0 };
     }
 
+    inline Bit eq(BVecSignalPort lhs, BVecSignalPort rhs) { return makeNode(hlim::Node_Compare::EQ, lhs, rhs); }
+    inline Bit neq(BVecSignalPort lhs, BVecSignalPort rhs) { return makeNode(hlim::Node_Compare::NEQ, lhs, rhs); }
+    inline Bit gt(BVecSignalPort lhs, BVecSignalPort rhs) { return makeNode(hlim::Node_Compare::GT, lhs, rhs); }
+    inline Bit lt(BVecSignalPort lhs, BVecSignalPort rhs) { return makeNode(hlim::Node_Compare::LT, lhs, rhs); }
+    inline Bit geq(BVecSignalPort lhs, BVecSignalPort rhs) { return makeNode(hlim::Node_Compare::GEQ, lhs, rhs); }
+    inline Bit leq(BVecSignalPort lhs, BVecSignalPort rhs) { return makeNode(hlim::Node_Compare::LEQ, lhs, rhs); }
 
-#define BUILD_OP_BVEC(cpp_op, nodeOP) \
-    inline Bit operator cpp_op (const BVec& l, const BVec& r) { SignalCompareOp op(nodeOP); return op(l, r); }
-    
-#define BUILD_OP_BIT(cpp_op, nodeOP) \
-    inline Bit operator cpp_op (const Bit& l, const Bit& r) { SignalCompareOp op(nodeOP); return op(l, r); }
+    inline Bit operator == (BVecSignalPort lhs, BVecSignalPort rhs) { return eq(lhs, rhs); }
+    inline Bit operator != (BVecSignalPort lhs, BVecSignalPort rhs) { return neq(lhs, rhs); }
+    inline Bit operator <= (BVecSignalPort lhs, BVecSignalPort rhs) { return leq(lhs, rhs); }
+    inline Bit operator >= (BVecSignalPort lhs, BVecSignalPort rhs) { return geq(lhs, rhs); }
+    inline Bit operator < (BVecSignalPort lhs, BVecSignalPort rhs) { return lt(lhs, rhs); }
+    inline Bit operator > (BVecSignalPort lhs, BVecSignalPort rhs) { return gt(lhs, rhs); }
 
-    BUILD_OP_BVEC(== , hlim::Node_Compare::EQ)
-    BUILD_OP_BVEC(!= , hlim::Node_Compare::NEQ)
-    BUILD_OP_BVEC(> , hlim::Node_Compare::GT)
-    BUILD_OP_BVEC(< , hlim::Node_Compare::LT)
-    BUILD_OP_BVEC(>= , hlim::Node_Compare::GEQ)
-    BUILD_OP_BVEC(<= , hlim::Node_Compare::LEQ)
 
-    BUILD_OP_BIT(== , hlim::Node_Compare::EQ)
-    BUILD_OP_BIT(!= , hlim::Node_Compare::NEQ)
-#undef BUILD_OP_BVEC
-#undef BUILD_OP_BIT
+    inline Bit eq(BitSignalPort lhs, BitSignalPort rhs) { return makeNode(hlim::Node_Compare::EQ, lhs, rhs); }
+    inline Bit neq(BitSignalPort lhs, BitSignalPort rhs) { return makeNode(hlim::Node_Compare::NEQ, lhs, rhs); }
 
-    inline Bit operator == (const Bit& l, bool r) { return r ? l : ~l; }
-    inline Bit operator == (bool l, const Bit& r) { return r == l; }
-
-    inline Bit operator != (const Bit& l, bool r) { return r ? ~l : l; }
-    inline Bit operator != (bool l, const Bit& r) { return r != l; }
-
+    inline Bit operator == (BitSignalPort lhs, BitSignalPort rhs) { return eq(lhs, rhs); }
+    inline Bit operator != (BitSignalPort lhs, BitSignalPort rhs) { return neq(lhs, rhs); }
 }
 
