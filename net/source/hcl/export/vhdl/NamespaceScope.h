9fca7df738ea543cf0ae85cd0f3ea988167bf717
Andreas Ley <mail@andreas-ley.com>
2021-01-12 11:29:16 +0100

    missing file

--- net/source/hcl/export/vhdl/NamespaceScope.h
+++ net/source/hcl/export/vhdl/NamespaceScope.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../../hlim/Node.h"
+#include "../../hlim/SignalGroup.h"
 
 #include "CodeFormatting.h"
 
@@ -9,6 +10,7 @@
 #include <map>
 #include <set>
 #include <string>
+#include <vector>
 
 
 namespace hcl::core::vhdl {
@@ -23,6 +25,17 @@ struct NodeInternalStorageSignal
     inline bool operator==(const NodeInternalStorageSignal &rhs) const { return node == rhs.node && signalIdx == rhs.signalIdx; }
     inline bool operator<(const NodeInternalStorageSignal &rhs) const { if (node < rhs.node) return true; if (node > rhs.node) return false; return signalIdx < rhs.signalIdx; }
 };
+
+
+struct TypeDefinition
+{
+    std::vector<hlim::SignalGroup*> signalGroups;
+    std::string typeName;
+    std::string desiredTypeName;
+
+    bool compatibleWith(hlim::SignalGroup *signalGroup);
+};
+
 
 /**
  * @todo write docs
@@ -42,6 +55,9 @@ class NamespaceScope
         std::string allocateName(hlim::Clock *clock, const std::string &desiredName);
         const std::string &getName(hlim::Clock *clock) const;
 
+        std::string allocateName(hlim::Node_Pin *ioPin, const std::string &desiredName);
+        const std::string &getName(hlim::Node_Pin *ioPin) const;
+
         std::string allocateEntityName(const std::string &desiredName);
         std::string allocateBlockName(const std::string &desiredName);
         std::string allocateProcessName(const std::string &desiredName, bool clocked);
@@ -54,6 +70,8 @@ class NamespaceScope
         std::map<hlim::NodePort, std::string> m_nodeNames;
         std::map<NodeInternalStorageSignal, std::string> m_nodeStorageNames;
         std::map<hlim::Clock*, std::string> m_clockNames;
+        std::map<hlim::Node_Pin*, std::string> m_ioPinNames;
+        std::vector<TypeDefinition> m_typeDefinitions;
 };
 
 
