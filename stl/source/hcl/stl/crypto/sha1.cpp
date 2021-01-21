9aaaac5d51183259b60de6bd8281012ce7175621
MiO <mio@mflour.com>
2021-01-21 20:53:18 +0100

    add md5

--- stl/source/hcl/stl/crypto/sha1.cpp
+++ stl/source/hcl/stl/crypto/sha1.cpp
@@ -1,7 +1,10 @@
 #include "sha1.h"
+#include "md5.h"
 #include "../Adder.h"
 
 template struct hcl::stl::Sha1Generator<>;
 template struct hcl::stl::Sha0Generator<>;
+
+template struct hcl::stl::Md5Generator<>;
 
 template class hcl::stl::HashEngine<hcl::stl::Sha1Generator<>>;
