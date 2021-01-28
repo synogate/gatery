995f9df437e6bc53080081ad2fe6e64f1c7f3952
Andreas Ley <mail@andreas-ley.com>
2021-01-28 14:32:27 +0100

    != operator for nodeport

--- net/source/hcl/hlim/NodePort.h
+++ net/source/hcl/hlim/NodePort.h
@@ -16,6 +16,7 @@ struct NodePort {
     size_t port = INV_PORT;
     
     inline bool operator==(const NodePort &rhs) const { return node == rhs.node && port == rhs.port; }
+    inline bool operator!=(const NodePort &rhs) const { return !operator==(rhs); }
     inline bool operator<(const NodePort &rhs) const { if (node < rhs.node) return true; if (node > rhs.node) return false; return port < rhs.port; }
 };
 
