cc36991d143091c35c7b14828bdd49210622cae0
Andreas Ley <mail@andreas-ley.com>
2021-01-28 15:09:01 +0100

    Disabled MD5 Generator because of missing rot implementation

--- stl/source/hcl/stl/crypto/sha1.cpp
+++ stl/source/hcl/stl/crypto/sha1.cpp
@@ -5,6 +5,6 @@
 template struct hcl::stl::Sha1Generator<>;
 template struct hcl::stl::Sha0Generator<>;
 
-template struct hcl::stl::Md5Generator<>;
+//template struct hcl::stl::Md5Generator<>;
 
 template class hcl::stl::HashEngine<hcl::stl::Sha1Generator<>>;
