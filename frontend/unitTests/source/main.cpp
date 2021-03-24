48abffe8a336c093c7e1d9e6289a0d8f4a94874b
MiO <mio@synogate.com>
2021-03-24 15:38:17 +0100

    msvc build fix

--- frontend/unitTests/source/main.cpp
+++ frontend/unitTests/source/main.cpp
@@ -1,5 +1,5 @@
 #define BOOST_TEST_MODULE "Unit tests for core library (including frontend)"
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include <hcl/frontend/FrontendUnitTestSimulationFixture.h>
 
