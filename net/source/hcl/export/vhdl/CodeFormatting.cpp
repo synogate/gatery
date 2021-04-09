5f9b67b6a5b9c7d6d6f08dc3a78bdf7b58685da9
Andreas Ley <andy@synogate.com>
2021-04-09 10:26:25 +0200

    Removed license reference from default vhdl export code formatting

--- net/source/hcl/export/vhdl/CodeFormatting.cpp
+++ net/source/hcl/export/vhdl/CodeFormatting.cpp
@@ -36,10 +36,9 @@ DefaultCodeFormatting::DefaultCodeFormatting()
     m_fileHeader =
 R"Delim(
 --------------------------------------------------------------------
--- This file is under some license that we haven't figured out yet.
--- Also it was auto generated. DO NOT MODIFY. Any changes made
--- directly can not be brought back into the source material and
--- will be lost uppon regeneration.
+-- This file was auto generated from Gatery. DO NOT MODIFY. Any
+-- changes made directly can not be brought back into the source
+-- material and will be lost uppon regeneration.
 --------------------------------------------------------------------
 )Delim";
 
