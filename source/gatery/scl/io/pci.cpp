70c491655b273f18fc6e174b5d95f9c1c142f157
MiO <mio@synogate.com>
2021-06-05 11:37:12 +0200

    add pci p tile state

---
+++ source/gatery/scl/io/pci.cpp
@@ -0,0 +1,201 @@
+/*  This file is part of Gatery, a library for circuit design.
+	Copyright (C) 2021 Michael Offel, Andreas Ley
+
+	Gatery is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 3 of the License, or (at your option) any later version.
+
+	Gatery is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include "gatery/pch.h"
+#include "pci.h"
+
+#include "../Fifo.h"
+#include "../StreamArbiter.h"
+
+gtry::Bit gtry::scl::pci::isCompletionTlp(const BVec& tlpHeader)
+{
+	HCL_DESIGNCHECK_HINT(tlpHeader.getWidth() >= 8_b, "first 8b of tlp header required for decoding");
+	Bit completion_tlp = tlpHeader(0, 5) == "b01010";
+	HCL_NAMED(completion_tlp);
+	return completion_tlp;
+}
+
+gtry::Bit gtry::scl::pci::isMemTlp(const BVec& tlpHeader)
+{
+	HCL_DESIGNCHECK_HINT(tlpHeader.getWidth() >= 8_b, "first 8b of tlp header required for decoding");
+	Bit mem_tlp = tlpHeader(0, 5) == 0;
+	HCL_NAMED(mem_tlp);
+	return mem_tlp;
+}
+
+gtry::Bit gtry::scl::pci::isDataTlp(const BVec& tlpHeader)
+{
+	HCL_DESIGNCHECK_HINT(tlpHeader.getWidth() >= 32_b, "first word of tlp header required for decoding");
+	Bit data_tlp = tlpHeader[30];
+	HCL_NAMED(data_tlp);
+	return data_tlp;
+}
+
+gtry::scl::pci::AvmmBridge::AvmmBridge(Stream<Tlp>& rx, AvalonMM& avmm, const PciId& cplId) :
+	AvmmBridge()
+{
+	setup(rx, avmm, cplId);
+}
+
+void gtry::scl::pci::AvmmBridge::setup(Stream<Tlp>& rx, AvalonMM& avmm, const PciId& cplId)
+{
+	HCL_DESIGNCHECK_HINT(rx.header.getWidth() == 96_b, "reduce tlp header address using discardHighAddressBits");
+	m_cplId = cplId;
+	generateFifoBridge(rx, avmm);
+}
+
+void gtry::scl::pci::AvmmBridge::generateFifoBridge(Stream<Tlp>& rx, AvalonMM& avmm)
+{
+	HCL_DESIGNCHECK(avmm.readData->getWidth() == 32_b);
+
+	auto entity = Area{ "AvmmBridge" }.enter();
+
+	sim_assert(!*rx.valid | rx.header(TlpOffset::type) == 0) << "not a memory tlp";
+
+	// decode command
+	avmm.address = rx.header(64, 32_b);
+	avmm.address(0, 2_b) = 0;
+
+	avmm.write = rx.transfer() & isDataTlp(rx.header);
+	avmm.read = rx.transfer() & !*avmm.write;
+	avmm.writeData = rx.data;
+
+	// store cpl data for command
+	size_t pipelineDepth = std::max( avmm.maximumPendingReadTransactions, 1ull );
+	Fifo<MemTlpCplData> resQueue{ pipelineDepth , MemTlpCplData{} };
+	
+	MemTlpCplData req;
+	req.decode(rx.header);
+	HCL_NAMED(req);
+	resQueue.push(req, *avmm.read);
+
+	// create tx tlp
+	avmm.createReadDataValid();
+	m_tx.valid = *avmm.readDataValid;
+	m_tx.header = "96x00000000000000044a000001";
+	m_tx.header(48, 16_b) = pack(m_cplId);
+	m_tx.data = *avmm.readData;
+
+	// join response and cpl data
+	MemTlpCplData res;
+	resQueue.pop(res, *m_tx.valid);
+	res.encode(m_tx.header);
+
+	*rx.ready = !resQueue.full();
+	if (avmm.ready)
+		*rx.ready &= *avmm.ready;
+
+}
+
+gtry::scl::pci::Tlp gtry::scl::pci::Tlp::discardHighAddressBits() const
+{
+	Tlp tlpHdr{
+		.prefix = prefix,
+		.header = header(0, 96_b),
+		.data = data
+	};
+
+	if(header.getWidth() > 96_b)
+		IF(header[TlpOffset::has4DW])
+			tlpHdr.header(64, 32_b) = header(96, 32_b);
+
+	HCL_NAMED(tlpHdr);
+	return tlpHdr;
+}
+
+void gtry::scl::pci::MemTlpCplData::decode(const BVec& tlpHdr)
+{
+	attr.trafficClass = tlpHdr(20, 3_b);
+	attr.idBasedOrdering = tlpHdr[18];
+	attr.relaxedOrdering = tlpHdr[13];
+	attr.noSnoop = tlpHdr[12];
+	lowerAddress = pack(tlpHdr(66, 5_b), "b00");
+
+	tag = tlpHdr(40, 8_b);
+	requester.func = tlpHdr(48, 3_b);
+	requester.dev = tlpHdr(48 + 3, 5_b);
+	requester.bus = tlpHdr(56, 8_b);
+}
+
+void gtry::scl::pci::MemTlpCplData::encode(BVec& tlpHdr) const
+{
+	tlpHdr(20, 3_b) = attr.trafficClass;
+	tlpHdr[18] = attr.idBasedOrdering;
+	tlpHdr[13] = attr.relaxedOrdering;
+	tlpHdr[12] = attr.noSnoop;
+
+	tlpHdr(64, 32_b) = pack(requester.bus, requester.dev, requester.func, tag, '0', lowerAddress);
+}
+
+void gtry::scl::pci::IntelPTileCompleter::generate()
+{
+	auto entity = Area{ "IntelPTileCompleter" }.enter();
+
+	Bit anyValid = '0';
+	std::array<Stream<Tlp>, 2> in_reduced;
+	for (size_t i = 0; i < in.size(); ++i)
+	{
+		in[i].ready.emplace();
+		in[i].valid.emplace();
+		in[i].sop.emplace();
+		in[i].eop.emplace();
+		in[i].header = 128_b;
+		in[i].data = 32_b;
+
+		BVec header = swapEndian(in[i].header, 8_b, 32_b);
+		IF(header[29]) // 4 DW tlp header
+			header(64, 32_b) = header(96, 32_b);
+
+		in_reduced[i].valid = *in[i].valid & *in[i].sop & *in[i].eop;
+		in_reduced[i].data = in[i].data;
+		in_reduced[i].header = header(0, 96_b);
+		anyValid |= *in_reduced[i].valid;
+	}
+
+	Fifo in_buffer(32, in_reduced);
+	in_buffer.push(in_reduced, anyValid);
+
+	*in[0].ready = in_buffer.almostEmpty(32 - 27);
+	for (size_t i = 1; i < in.size(); ++i)
+		*in[i].ready = *in[0].ready;
+
+	
+	std::array<Stream<Tlp>, 2> in_buffered;
+	Bit in_buffer_pop;
+	in_buffer.pop(in_buffered, in_buffer_pop);
+
+	arbitrateInOrder in_single{ in_buffered[0], in_buffered[1] };
+	in_buffer_pop = in_buffered[0].transfer() | in_buffered[1].transfer();
+
+	AvalonMM avmm;
+	avmm.ready = Bit{};
+	avmm.read = Bit{};
+	avmm.write = Bit{};
+	avmm.writeData = 32_b;
+	avmm.readData = 32_b;
+	avmm.readDataValid = Bit{};
+
+	AvmmBridge bridge{ in_single, avmm, completerId };
+
+	Fifo<Stream<Tlp>> out_buffer{ 16, bridge.tx() };
+	out_buffer.push(bridge.tx(), bridge.tx().transfer());
+
+
+	
+
+
+}
---
+++ tests/scl/pci_test.cpp
@@ -0,0 +1,222 @@
+/*  This file is part of Gatery, a library for circuit design.
+    Copyright (C) 2021 Michael Offel, Andreas Ley
+
+    Gatery is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3 of the License, or (at your option) any later version.
+
+    Gatery is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include "scl/pch.h"
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/dataset.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+
+#include <gatery/simulation/waveformFormats/VCDSink.h>
+
+#include <gatery/scl/io/pci.h>
+
+#include <queue>
+
+using namespace boost::unit_test;
+using namespace gtry;
+
+struct TbReq
+{
+    bool read = false;
+    bool write = false;
+    uint32_t address = 0;
+    uint32_t data = 0;
+};
+
+struct TlpBuilder
+{
+    uint64_t hdr;
+
+    TlpBuilder& length(size_t words) { return insert(words, 0, 10); }
+    TlpBuilder& data(bool val = true) { return insert(val, 30); }
+    TlpBuilder& requester(size_t bus, size_t dev, size_t func) {
+        insert(func, 48, 3);
+        insert(dev, 48 + 3, 5);
+        return insert(bus, 56, 8);
+    }
+    TlpBuilder& tag(size_t val) { return insert(val, 40, 8); }
+
+    operator uint64_t () const { return hdr; }
+
+    TlpBuilder& insert(uint64_t val, size_t offset, size_t width) 
+    { 
+        hdr = gtry::utils::bitfieldInsert(hdr, offset, width, val);
+        return *this;
+    }
+
+    TlpBuilder& insert(bool val, size_t offset)
+    {
+        return insert(val ? 1 : 0, offset, 1);
+    }
+};
+
+BOOST_FIXTURE_TEST_CASE(pci_AvmmBridge_basic, UnitTestSimulationFixture)
+{
+    Clock clock(ClockConfig{}.setAbsoluteFrequency(100'000'000).setName("clock"));
+    ClockScope clkScp(clock);
+
+    scl::Stream<scl::pci::Tlp> in;
+    in.valid = pinIn().setName("in_valid");
+
+    BVec inHeader = pinIn(64_b).setName("in_header");
+    BVec inAddress = pinIn(32_b).setName("in_address");
+    in.header = pack(inAddress, inHeader);
+    in.data = pinIn(32_b).setName("in_data");
+    in.ready = Bit{};
+    pinOut(*in.ready).setName("in_ready");
+
+    scl::pci::PciId completerId;
+    completerId.bus = 1;
+    completerId.dev = 2;
+    completerId.func = 3;
+
+    scl::AvalonMM avmm;
+    avmm.ready = Bit{};
+    avmm.address = 16_b;
+    avmm.read = Bit{};
+    avmm.write = Bit{};
+    avmm.writeData = 32_b;
+    avmm.readData = 32_b;
+    avmm.readDataValid = Bit{};
+    avmm.maximumPendingReadTransactions = 4;
+
+    scl::pci::AvmmBridge uut{ in, avmm, completerId };
+    scl::Stream<scl::pci::Tlp>& out = uut.tx();
+    out.ready = pinIn().setName("out_ready");
+    pinOut(*out.valid).setName("out_valid");
+    pinOut(out.header).setName("out_header");
+    pinOut(out.data).setName("out_data");
+
+    Memory<BVec> testMem{ avmm.address.getWidth().count(), avmm.writeData->getWidth() };
+    IF(*avmm.write & *avmm.ready)
+        testMem[avmm.address] = *avmm.writeData;
+    *avmm.readData = testMem[avmm.address];
+    *avmm.readDataValid = *avmm.read;
+    for (size_t i = 0; i < 6; ++i)
+    {
+        *avmm.readData = reg(*avmm.readData);
+        *avmm.readDataValid = reg(*avmm.readDataValid, '0');
+    }
+    *avmm.ready = pinIn().setName("avmm_ready_sim");
+
+    setName(avmm, "avmm");
+    //avmm.pinOut("avmm");
+
+    // stream in bfm
+    std::queue<TbReq> reqQueue;
+    std::queue<TlpBuilder> cplQueue;
+    addSimulationProcess([&]()->SimProcess {
+
+        std::mt19937 rng{ 18057 };
+        uint8_t tag = 0xAA;
+
+        simu(*in.valid) = 0;
+        while (true)
+        {
+            if (simu(*in.ready) != 0)
+            {
+                if (reqQueue.empty())
+                {
+                    simu(*in.valid) = 0;
+                }
+                else
+                {
+                    const TbReq req = reqQueue.front();
+                    reqQueue.pop();
+                    simu(inAddress) = req.address;
+                    simu(in.data) = req.data;
+
+                    auto tlp = TlpBuilder{}.length(1);
+                    tlp.requester(rng(), rng(), rng());
+                    tlp.tag(tag++);
+
+                    if (req.read)
+                    {
+                        simu(*in.valid) = 1;
+                        simu(inHeader) = tlp;
+                        cplQueue.push(tlp);
+                    }
+                    else if (req.write)
+                    {
+                        simu(*in.valid) = 1;
+                        simu(inHeader) = tlp.data();
+                    }
+                    else
+                    {
+                        simu(*in.valid) = 0;
+                    }
+                }
+            }
+            co_await WaitClk(clock);
+        }
+    });
+
+    // stream out bfm
+    addSimulationProcess([&]()->SimProcess {
+        simu(*out.ready) = 1;
+
+        while (true)
+        {
+            if (simu(*out.ready) & simu(*out.valid))
+            {
+                BOOST_TEST(!cplQueue.empty());
+
+                if (!cplQueue.empty())
+                {
+                    uint64_t reqTlp = cplQueue.front();
+                    cplQueue.pop();
+
+                    auto resTlpState = simu(out.header).eval();
+                    uint64_t hdr = resTlpState.extractNonStraddling(sim::DefaultConfig::VALUE, 0, 64);
+                    uint64_t dst = resTlpState.extractNonStraddling(sim::DefaultConfig::VALUE, 64, 32);
+                    uint64_t data = simu(out.data);
+
+                    BOOST_TEST(gtry::utils::bitfieldExtract(hdr, 24, 8) == 0x4A);
+
+                }
+            }
+            co_await WaitClk(clock);
+        }
+
+
+    });
+
+    addSimulationProcess([&]()->SimProcess {
+        simu(*avmm.ready) = 1;
+
+        co_await WaitClk(clock);
+
+        for (uint32_t i = 0; i < 16; ++i)
+            reqQueue.push(TbReq{ .write = true, .address = i * 4, .data = i });
+
+        for (uint32_t i = 0; i < 16; ++i)
+            reqQueue.push(TbReq{ .read = true, .address = i * 4 });
+
+
+    });
+
+
+    sim::VCDSink vcd{ design.getCircuit(), getSimulator(), "pci_AvmmBridge_basic.vcd" };
+    vcd.addAllPins();
+    vcd.addAllNamedSignals();
+
+    design.getCircuit().postprocess(gtry::DefaultPostprocessing{});
+    design.visualize("pci_AvmmBridge_basic");
+
+    runTicks(clock.getClk(), 190);
+}
