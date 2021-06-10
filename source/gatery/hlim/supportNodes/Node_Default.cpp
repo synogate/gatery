dab216d228ca49fe06f58be211a3205c33f3edd2
Andreas Ley <andy@synogate.com>
2021-06-10 14:32:31 +0200

    added missing prcompiled header include

--- source/gatery/hlim/supportNodes/Node_Default.cpp
+++ source/gatery/hlim/supportNodes/Node_Default.cpp
@@ -16,6 +16,8 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#include "gatery/pch.h"
+
 #include "Node_Default.h"
 
 namespace gtry::hlim {
--- source/gatery/hlim/supportNodes/Node_ExportOverride.cpp
+++ source/gatery/hlim/supportNodes/Node_ExportOverride.cpp
@@ -16,6 +16,8 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#include "gatery/pch.h"
+
 #include "Node_ExportOverride.h"
 
 namespace gtry::hlim {
