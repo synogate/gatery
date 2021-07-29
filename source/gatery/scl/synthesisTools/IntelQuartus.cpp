c75fcb43198ae0faaebb76a104bbffff70173a13
Andreas Ley <andy@synogate.com>
2021-07-29 10:35:04 +0200

    Quartus import tcl script fixed

--- source/gatery/scl/synthesisTools/IntelQuartus.cpp
+++ source/gatery/scl/synthesisTools/IntelQuartus.cpp
@@ -105,19 +105,41 @@ void IntelQuartus::writeVhdlProjectScript(vhdl::VHDLExport &vhdlExport, std::str
 	std::fstream file((vhdlExport.getDestination() / filename).string().c_str(), std::fstream::out);
 	file.exceptions(std::fstream::failbit | std::fstream::badbit);
 	file << R"(
-# Warning: untested!
+# This script is intended for adding the core to an existing project
+#     1. Open the quartus tcl console (View->Utility Windows->Tcl Console) 
+#     2. If necessary, change the current working directory to project directory ("cd [get_project_directory]")
+#     3. Source this script ("source path/to/this/script.tcl"). Use a relative path to this script if you want files to be added with relative paths (recommended).
+
+
 package require ::quartus::project
+
+set projectDirectory [get_project_directory]
+set currentDirectory [pwd]/
+
+if {$currentDirectory != $projectDirectory} {
+	puts "The current working directory must be the project directory!"
+	puts "Current working directory: $currentDirectory"
+	puts "Project directory: $projectDirectory"
+} else {
+	variable scriptLocation [info script]
+	set directory [file dirname $scriptLocation]
+	set pathType [file pathtype $directory]
+
+	puts "The path to the script and files seems to be $directory so prepending filenames with this path."
+
+	if {$pathType != "relative"} {
+		puts "Warning: The files are prefixed with an absolute path which breaks the project if it is moved around. Source this tcl script with a relative path if you want relative paths in your project file!"
+	}
 
 )" << std::endl;
 
 	auto&& sdcFile = vhdlExport.getConstraintsFilename();
 	if (!sdcFile.empty())
-		file << "set_global_assignment -name SDC_FILE " << sdcFile << '\n';
+		file << "    set_global_assignment -name SDC_FILE $directory/" << sdcFile << '\n';
 
 	for (std::filesystem::path& vhdl_file : sourceFiles(vhdlExport, true, false))
 	{
-		file << "set_global_assignment -name VHDL_FILE ";
-		file << vhdl_file.string();
+		file << "    set_global_assignment -name VHDL_FILE -hdl_version VHDL_2008 $directory/" << vhdl_file.string();
 		if (!vhdlExport.getName().empty())
 			file << " -library " << vhdlExport.getName();
 		file << '\n';
@@ -126,9 +148,9 @@ package require ::quartus::project
 //	vhdlExport.writeXdc("clocks.xdc");
 
 	file << R"(
-export_assignments
+    export_assignments
+}
 )";
-
 }
 
 	void IntelQuartus::writeStandAloneProject(vhdl::VHDLExport& vhdlExport, std::string_view filename)
