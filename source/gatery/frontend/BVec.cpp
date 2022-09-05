dc19bdbb704f80583f99a1006290bdf15d861285
MiO <mio@synogate.com>
2022-09-05 16:28:58 +0200

    replace ext(BVec,...

--- source/gatery/frontend/BVec.cpp
+++ source/gatery/frontend/BVec.cpp
@@ -18,15 +18,36 @@
 #include "gatery/pch.h"
 #include "BVec.h"
 
-namespace gtry {
+namespace gtry 
+{
+	BVec ext(const BVec& bvec, BitWidth extendedWidth, Expansion policy)
+	{
+		HCL_DESIGNCHECK_HINT(extendedWidth.bits() >= bvec.size(), "ext is not allowed to reduce width");
 
-	BVec ext(const BVec& bvec, size_t increment, Expansion policy)
+		SignalReadPort port = bvec.readPort();
+		port.expansionPolicy = policy;
+		if (extendedWidth > bvec.width())
+			port = port.expand(extendedWidth.bits(), hlim::ConnectionType::BITVEC);
+		return BVec(port);
+	}
+
+	BVec ext(const BVec& bvec, BitExtend increment, Expansion policy)
 	{
 		SignalReadPort port = bvec.readPort();
 		port.expansionPolicy = policy;
-		if (increment)
-			port = port.expand(bvec.size() + increment, hlim::ConnectionType::BITVEC);
+		if (increment.value)
+			port = port.expand(bvec.size() + increment.value, hlim::ConnectionType::BITVEC);
 		return BVec(port);
 	}
-	
+
+	BVec ext(const BVec& bvec, BitReduce decrement, Expansion policy)
+	{
+		HCL_DESIGNCHECK(decrement.value >= bvec.size());
+
+		SignalReadPort port = bvec.readPort();
+		port.expansionPolicy = policy;
+		if (decrement.value)
+			port = port.expand(bvec.size() - decrement.value, hlim::ConnectionType::BITVEC);
+		return BVec(port);
+	}
 }
