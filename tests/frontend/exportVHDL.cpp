081ba4fb02355436f62aae7c69c7092d03efa041
Andreas Ley <andy@synogate.com>
2022-09-27 13:47:20 +0200

    fixed broken pch include

--- tests/frontend/exportVHDL.cpp
+++ tests/frontend/exportVHDL.cpp
@@ -15,7 +15,7 @@
 	License along with this library; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
-#include "scl/pch.h"
+#include "frontend/pch.h"
 
 
 #include <gatery/frontend/GHDLTestFixture.h>
