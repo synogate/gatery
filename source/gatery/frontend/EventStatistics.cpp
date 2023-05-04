d46f8d581dc4cef7de3e758557ab88a5a34006ed
Johannes Ehlert <johannes.ehlert@synogate.com>
2023-05-04 10:37:33 +0200

    Functional implementation of EventStatistics

---
+++ source/gatery/frontend/EventStatistics.cpp
@@ -0,0 +1,98 @@
+/*  This file is part of Gatery, a library for circuit design.
+	Copyright (C) 2021 Michael Offel, Andreas Ley
+
+	Gatery is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 3 of the License, or (at your option) any later version.
+
+	Gatery is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include "gatery/pch.h"
+//#include "gatery/pch.h"
+
+//#include "Bit.h"
+//#include "BVec.h"
+//#include "ConditionalScope.h"
+//#include "Constant.h"
+//#include "DesignScope.h"
+//#include "PipeBalanceGroup.h"
+//#include "Reg.h"
+#include "DesignScope.h"
+//#include "SignalCompareOp.h"
+#include "Clock.h"
+#include "EventStatistics.h"
+#include "SimSigHandle.h"
+
+
+
+namespace gtry {
+
+	void EventStatistics::addEvent(std::string_view name, Bit trigger) {
+		auto clk = ClockScope::getClk();
+		auto pathName = getNodePath(name);
+		m_counter[pathName] = 0;
+
+		DesignScope::get()->getCircuit().addSimulationProcess([=]()->SimProcess {
+			while (true)
+			{
+				co_await OnClk(clk);
+				if (simu(trigger) == '1')
+					m_counter[pathName] += 1;
+			}
+			});
+		
+		
+	};
+
+	void EventStatistics::dumpStatistics() {
+		std::cout << "Signal statistics" << std::endl;
+		for (const auto& elems : m_counter)
+			std::cout << elems.first << "|" << elems.second << std::endl;
+	};
+
+	size_t EventStatistics::readEventCounter(std::string_view name) {
+		auto itCounter = m_counter.find(name);
+		HCL_DESIGNCHECK_HINT(itCounter != m_counter.end(), "An event counter with this name was never registered");
+		return itCounter->second;
+	};
+
+	void EventStatistics::writeStatTable(std::filesystem::path file_name) {
+		std::ofstream file;
+		file.open(file_name.string());
+		file << "Signal name;Counter value;\n";
+		for (const auto& elems : m_counter)
+			file << elems.first << ";" << elems.second << ";\n";
+		file.close();
+		std::cout << "Statistic table written to " << file_name << std::endl;
+	};
+
+	std::string EventStatistics::getNodePath(std::string_view name)
+	{
+		auto currentEntity = GroupScope::getCurrentNodeGroup();
+		std::string pathName = std::string(name);
+
+		while (currentEntity != nullptr)
+		{
+			pathName = currentEntity->getInstanceName()+ "/" + pathName;
+			currentEntity = currentEntity->getParent();
+		}
+
+		return pathName;
+	};
+
+	void registerEvent(std::string_view name, Bit trigger) {
+
+		EventStatistics::get()->addEvent(name, trigger);
+
+	}
+
+	
+}
---
+++ source/gatery/frontend/EventStatistics.h
@@ -0,0 +1,69 @@
+/*  This file is part of Gatery, a library for circuit design.
+	Copyright (C) 2021 Michael Offel, Andreas Ley
+
+	Gatery is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 3 of the License, or (at your option) any later version.
+
+	Gatery is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#pragma once
+
+//#include "Signal.h"
+#include "Bit.h"
+#include <filesystem>
+//#include <gatery/frontend.h>
+#include "Scope.h"
+#include <functional>
+//#include <gatery/utils.h>
+//#include <gatery/hlim/NodePtr.h>
+//#include <gatery/utils/Exceptions.h>
+//#include <gatery/utils/Traits.h>
+
+//#include <vector>
+//#include <optional>
+
+
+
+
+
+/**
+ * @addtogroup gtry_signals
+ * @{
+ */
+ 
+	
+	
+
+	namespace gtry{
+
+	/**
+	 * @brief Event counter for attached Signals
+	 * @details ...
+	 */
+class EventStatistics : public BaseScope<EventStatistics> {
+public:
+	void addEvent(std::string_view name, Bit trigger);
+	void dumpStatistics();
+	size_t readEventCounter(std::string_view name);
+	void writeStatTable(std::filesystem::path file_name);
+	static EventStatistics* get() { return m_currentScope; }
+
+protected:
+	std::map<std::string, size_t, std::less<>> m_counter;
+	std::string getNodePath(std::string_view name);
+};
+
+void registerEvent(std::string_view name, Bit trigger);
+
+/**@}*/
+
+}
