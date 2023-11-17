b1ffce8a95e36c26b36d5970bb88e862edb17c5c
Andreas Ley <andy@synogate.com>
2023-11-17 20:17:51 +0100

    Fixed bug in BitVectorState resize function

--- source/gatery/simulation/BitVectorState.h
+++ source/gatery/simulation/BitVectorState.h
@@ -567,8 +567,8 @@ void BitVectorState<Config>::resize(size_t size)
 	{
 		m_values[i].resize((size + Config::NUM_BITS_PER_BLOCK - 1) / Config::NUM_BITS_PER_BLOCK);
 
-		if (size)
-			m_values[i].back() &= utils::bitMaskRange(0, size % (Config::NUM_BITS_PER_BLOCK + 1));
+		if (size & Config::NUM_BITS_PER_BLOCK != 0)
+			m_values[i].back() &= utils::bitMaskRange(0, size % Config::NUM_BITS_PER_BLOCK);
 	}
 }
 
