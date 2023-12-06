a2f63e93b0d37ca69aeaf366ddd2bf1da268c5a4
Leonel Gouveia Ergin <leonel.ge@synogate.com>
2023-12-06 14:03:56 +0100

    introduced the scl/io/pci folder

--- source/gatery/scl/arch/xilinx/XilinxPci.h
+++ source/gatery/scl/arch/xilinx/XilinxPci.h
@@ -17,7 +17,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 #pragma once
 #include <gatery/frontend.h>
-#include <gatery/scl/io/pci.h>
+#include <gatery/scl/io/pci/pci.h>
 #include "XilinxPciStructs.h"
 
 namespace gtry::scl::pci::xilinx {
