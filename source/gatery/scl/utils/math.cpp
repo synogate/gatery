f2ec05923679fadbf4b60b437b2a402a09e31873
Leonel Gouveia Ergin <leonel.ge@synogate.com>
2023-12-08 12:36:33 +0100

    added math::min and math::max functions

---
+++ source/gatery/scl/utils/math.cpp
@@ -0,0 +1,36 @@
+/*  This file is part of Gatery, a library for circuit design.
+Copyright (C) 2021 Michael Offel, Andreas Ley
+
+Gatery is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+Gatery is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include <gatery/pch.h>
+#include "math.h"
+
+namespace gtry::scl::math {
+	UInt min(const UInt& a, const UInt& b){
+		BitWidth retW = BitWidth(std::min(a.width().bits(), b.width().bits()));
+		UInt ret = retW;
+		ret = a;
+		IF(a > b) ret = b;
+		return ret;
+	}
+	UInt max(const UInt& a, const UInt& b) {
+		BitWidth retW = BitWidth(std::max(a.width().bits(), b.width().bits()));
+		UInt ret = retW;
+		ret = a;
+		IF(a < b) ret = b;
+		return ret;
+	}
+}
