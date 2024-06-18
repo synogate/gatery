e8711fa7859183f2999d57a88c88464e1da79228
Leonel Gouveia Ergin <leonel.ge@synogate.com>
2024-06-18 14:26:42 +0200

    added dirty method since we have robust function

--- source/gatery/scl/io/RecoverDataDifferential.cpp
+++ source/gatery/scl/io/RecoverDataDifferential.cpp
@@ -22,36 +22,45 @@
 
 gtry::scl::VStream<gtry::UInt> gtry::scl::recoverDataDifferential(const gtry::Clock &signalClock, Bit ioP, Bit ioN)
 {
+
 	auto scope = Area{ "scl_recoverDataDifferential" }.enter();
 
 	const auto samplesRatio = ClockScope::getClk().absoluteFrequency() / signalClock.absoluteFrequency();
 	HCL_DESIGNCHECK_HINT(samplesRatio.denominator() == 1, "clock must be divisible by signalClock");
 	const size_t samples = samplesRatio.numerator();
-	HCL_DESIGNCHECK_HINT(samples >= 3, "we need at least 3 samples per cycle to recover data");
 
 	ioP.resetValue('0');
 	ioN.resetValue('1');
-	// avoid meta stable inputs
-	Bit p = synchronize(ioP, signalClock, ClockScope::getClk(), {.outStages = 3, .inStage = false});
-	Bit n = synchronize(ioN, signalClock, ClockScope::getClk(), {.outStages = 3, .inStage = false});
-	HCL_NAMED(p);
-	HCL_NAMED(n);
 
-	Counter phaseCounter{ samples };
-	phaseCounter.inc();
-	
-	// sample data based on clock estimate
-	VStream<UInt> out{
-		cat(n, p),
-		Valid{ phaseCounter.isLast() }
-	};
+	VStream<UInt> out;
+	if(samples == 1) { //"dirty" method
+		// avoid meta stable inputs with dirty synchronizer
+		Bit p = reg(allowClockDomainCrossing(ioP, signalClock, ClockScope::getClk())); HCL_NAMED(p);
+		Bit n = reg(allowClockDomainCrossing(ioN, signalClock, ClockScope::getClk())); HCL_NAMED(n);
 
-	// recover clock and shift sample point
-	IF(p != reg(p, '1') | n != reg(n, '0'))
-	{
-		phaseCounter.load((samples + 1) / 2);
-		valid(out) = '0'; // prevent double sampling
+		*out = cat(n, p);
+		valid(out) = '1';
 	}
+	else {
+		HCL_DESIGNCHECK_HINT(samples >= 3, "we need at least 3 samples per cycle to recover data with the current oversampling method");
+		// avoid meta stable inputs
+		Bit p = synchronize(ioP, signalClock, ClockScope::getClk(), {.outStages = 3, .inStage = false}); HCL_NAMED(p);
+		Bit n = synchronize(ioN, signalClock, ClockScope::getClk(), {.outStages = 3, .inStage = false}); HCL_NAMED(n);
+
+		Counter phaseCounter{ samples };
+
+		// sample data based on clock estimate
+		*out = cat(n, p);
+		valid(out) = phaseCounter.isLast();
+
+		//recover clock and shift sample point
+		IF(p != reg(p, '1') | n != reg(n, '0'))
+		{
+			phaseCounter.load((samples + 1) / 2);
+			valid(out) = '0'; // prevent double sampling
+		}
+	}
+
 	HCL_NAMED(out);
 	return out;
 }
