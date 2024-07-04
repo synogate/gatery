8d8a4d8786ab4e7411fb359879d7f59866ea913e
Leonel Gouveia Ergin <leonel.ge@synogate.com>
2024-07-04 16:37:19 +0200

    moved counter logic to cpp made counter shorthand for updown clamped clounter

--- source/gatery/scl/Counter.h
+++ source/gatery/scl/Counter.h
@@ -23,115 +23,31 @@ namespace gtry::scl
 	class Counter
 	{
 	public:
+		Counter(size_t end, size_t startupValue = 0);
+		Counter(UInt end, size_t startupValue = 0);
+		Counter(BitWidth ctrW, size_t startupValue = 0);
 
-		Counter(size_t end, size_t startupValue = 0) :
-			m_area{ "scl_Counter", true }
-		{
-			if (!utils::isPow2(end)) {
-				init(end, BitWidth::last(end), true, startupValue);
-			}
-			else {
-				init(end, BitWidth::count(end), false, startupValue);
-			}
-			m_area.leave();
-		}
+		Counter& inc();
+		Counter& dec();
+		void reset();
+		void load(UInt value);
 
-		Counter(UInt end, size_t startupValue = 0) :
-			m_area{ "scl_Counter", true}
-		{
-			init(end, end.width(), true, startupValue);
-
-			m_area.leave();
-		}
-
-		Counter(BitWidth ctrW, size_t startupValue = 0) :
-			m_area{ "scl_Counter", true}
-		{
-			init(ctrW.count(), ctrW, true, startupValue);
-
-			m_area.leave();
-		}
-		
-		Counter& inc() { 
-			m_inc = '1';
-			if (gtry::ConditionalScope ___condScope{'1', true})
-				m_incrementNeverUsed &= '0';
-			return *this;
-		}
-
-		Counter& dec() {
-			m_dec = '1';
-			if (gtry::ConditionalScope ___condScope{'1', true})
-				m_incrementNeverUsed &= '0';
-			return *this;
-		}
-
-		void reset() { load(0); }
-		const UInt& value() const { return m_value; }
-		const Bit& isLast() const { return m_last; }
-		Bit isFirst() const { return m_value == 0; }
-		Bit becomesFirst() const { return m_becomesFirst; }
-
-		void load(UInt value) { m_load = '1'; m_loadValue = value; }
+		inline const UInt& value() const { return m_value; }
+		inline const Bit& isLast() const { return m_last; }
+		inline Bit isFirst() const { return m_value == 0; }
+		inline Bit becomesFirst() const { return m_becomesFirst; }
 	protected:
-		void init(UInt end, BitWidth counterW, bool checkOverflows, size_t resetValue = 0)
-		{
-			m_value = counterW;
-			m_loadValue = counterW;
-
-			HCL_NAMED(m_inc);
-			HCL_NAMED(m_dec);
-
-			m_last = m_value == (end - 1).lower(counterW);
-
-			if (counterW != BitWidth(0)) {
-				UInt delta = ConstUInt(0, m_value.width());
-				IF(m_incrementNeverUsed)
-					delta = 1;    // +1, auto-increment by default
-				IF(m_inc & !m_dec)
-					delta = 1;    // +1
-				IF(m_dec & !m_inc)
-					delta |= '1'; // -1
-				Bit isFirst = m_value == 0;
-				m_value += delta;
-				if (checkOverflows) {
-					IF(delta == 1)
-						IF(m_last)
-							m_value = 0;
-		
-					IF(delta == delta.width().mask())
-						IF(isFirst)
-							m_value = (end - 1).lower(counterW);
-				}
-			}
-			HCL_NAMED(m_load);
-			HCL_NAMED(m_loadValue);
-			IF(m_load)
-			{
-				m_value = m_loadValue;
-			}
-			m_becomesFirst = m_value == 0;
-			m_value = reg(m_value, resetValue, {.allowRetimingBackward = true, .allowRetimingForward = true});
-			HCL_NAMED(m_value);
-			HCL_NAMED(m_last);
-
-			m_load = '0';
-			m_inc = '0';
-			m_dec = '0';
-			m_incrementNeverUsed = '1';
-			m_loadValue = ConstUInt(m_loadValue.width());
-		}
-
+		void init(UInt end, BitWidth counterW, bool checkOverflows, size_t resetValue = 0);
 
 	private:
 		Area m_area;
 
 		UInt m_value;
 		Bit m_last;
-
 		Bit m_becomesFirst;
 
 		UInt m_loadValue;
+		size_t m_resetValue = 0;
 		Bit m_load;
 
 		Bit m_inc;
@@ -141,4 +57,5 @@ namespace gtry::scl
 
 	};
 
+	UInt counterUpDown(Bit increment, Bit decrement, Bit reset, BitWidth ctrW, size_t resetValue = 0);
 }
