3f39d916c5f40c5221810ac1a10179b7d56b4a0a
Leonel Gouveia Ergin <leonel.ge@synogate.com>
2024-07-09 15:40:52 +0200

    added recover data differential for sky 130

---
+++ source/gatery/scl/arch/sky130/recoverDataDifferential.h
@@ -0,0 +1,26 @@
+/*  This file is part of Gatery, a library for circuit design.
+Copyright (C) 2021 Michael Offel, Andreas Ley
+
+Gatery is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+Gatery is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#pragma once
+
+#include <gatery/frontend.h>
+#include <gatery/scl/stream/Stream.h>
+#include <gatery/scl/io/differential.h>
+
+namespace gtry::scl::arch::sky130 {
+	VStream<Bit, SingleEnded> recoverDataDifferentialEqualsamplingSky130(const Clock& signalClock, Bit ioP, Bit ioN);
+}
