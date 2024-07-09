f0512b7d99e4e132c4663b43dc4f611c7a69e506
Leonel Gouveia Ergin <leonel.ge@synogate.com>
2024-07-09 15:37:35 +0200

    added sky 130 cells

---
+++ source/gatery/scl/arch/sky130/delay.cpp
@@ -0,0 +1,35 @@
+/*  This file is part of Gatery, a library for circuit design.
+Copyright (C) 2021 Michael Offel, Andreas Ley
+
+Gatery is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+Gatery is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include <gatery/scl_pch.h>
+#include "delay.h"
+#include <gatery/scl/io/dynamicDelay.h>
+
+namespace gtry::scl::arch::sky130
+{
+	dlygate4sd3::dlygate4sd3(Strength strength, picoseconds simDelay, Library lib)
+		: ExternalModule("sky130_fd_sc_" + toLibraryName(lib) + "_dlygate4sd3_" + std::to_string((size_t)strength)),
+		m_simDelay(simDelay)
+	{}
+
+	Bit dlygate4sd3::operator()(Bit in) {
+		this->in("A") = in;
+		Bit ret = this->out("x");
+		ret.simulationOverride(simulateDelay(in, m_simDelay, "sim_delay")); //not perfect, but ok
+		return ret;
+	}
+};
